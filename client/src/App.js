2c52e4cad60f47490588bb70f90535de77aa4942
Jebbla <jlp2722@gmail.com>
2019-09-24 13:41:49 -0500

    models and controllers

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Nav from './components/Nav';
 import Jumbotron from './components/Jumbotron';
 import Search from './components/Search';
+import Results from './pages/Results';
+import Saved from './pages/Saved';
+import NoMatch from './pages/NoMatch';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div>
         <Nav />
         <Jumbotron />
-        <Search />
+        <Switch>
+        <Route exact path="/" component={Search} />
+        <Route exact path="/results" component={Results} />
+        <Route exact path="/saved" component={Saved} />
+        <Route component={NoMatch} />
+        </Switch>
       </div>
+      </Router>
     );
   }
 }
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
-const path = require("path");
 const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+//Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
+//Api Routes
+app.use(routes);
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
