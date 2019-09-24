558e8be1aff4b0aa54811fa1903082b295ecc593
Jebbla <jlp2722@gmail.com>
2019-09-23 21:18:16 -0500

    Added react router and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import "./App.css";
 import Nav from './components/Nav';
 import Jumbotron from './components/Jumbotron';
+import Search from './components/Search';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Nav />
         <Jumbotron />
+        <Search />
       </div>
     );
   }
--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -5,7 +5,7 @@ function Jumbotron() {
     return (
         <div className="container mt-4">
             <div className="jumbotron text-center bg-dark text-white">
-                <h1 className="display-4">Books Search</h1>
+                <h1 className="display-4">Book Search</h1>
                 <p className="lead">Search and Save Books</p>
             </div>
         </div>
