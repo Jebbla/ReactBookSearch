d7e7062a0e5fbfaea1805ba39ebe3d8fea573969
Jebbla <jlp2722@gmail.com>
2019-09-27 19:35:08 -0500

    An attempt for Heroku

--- server.js
+++ server.js
@@ -1,28 +1,60 @@
+// const express = require("express");
+// const PORT = process.env.PORT || 3001;
+// const mongoose = require("mongoose");
+// const routes = require("./routes");
+// const path = require("path");
+// const app = express();
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+// //Connect to MongoDB
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
+// //Api Routes
+// app.use(routes);
+// // Send every request to the React app
+// // Define any API routes before this runs
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+// app.listen(PORT, function() {
+//   console.log(`🌎 ==> API server now on port ${PORT}!`);
+// });
+
 const express = require("express");
-const PORT = process.env.PORT || 3001;
+
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const path = require("path");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+// Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Serve up static assets (usually on heroku)
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-//Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
-//Api Routes
+// Add routes, both API and view
 app.use(routes);
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
